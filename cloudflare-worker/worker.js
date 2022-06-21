dfb719753cfcdadaa1d53b1a7556f9f2cabbdba4
Michel Zehnder <MichelZ@users.noreply.github.com>
2022-06-21 11:47:40 +0200

    Add Workflow

---
+++ cloudflare-worker/worker.js
@@ -0,0 +1,31 @@
+import { Octokit } from "@octokit/core";
+
+const octokit = new Octokit({
+  auth: GITHUB_TOKEN
+})
+
+addEventListener("fetch", event => {
+    event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+    const headers = request.headers
+
+    const res1 = await octokit.request('POST /repos/BitnamiMirror/index/merge-upstream', {
+        owner: 'bitnami',
+        repo: 'charts',
+        branch: 'index'
+    })
+    
+    console.log(res1)
+    const res2 = await octokit.request('POST /repos/BitnamiMirror/index/merge-upstream', {
+        owner: 'bitnami',
+        repo: 'charts',
+        branch: 'master'
+    })
+    
+    console.log(res2)
+    return new Response(`{ "ok": true }`, {
+        headers: { "content-type": "application/json" },
+    });
+}
